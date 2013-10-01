b77afd4bc843ae0619288bd4c5fb90ccdf0f1044
Steven Kester <a0080415@nus.edu.sg>
2013-10-01 23:59:15 +0800

    added display parse tried add and display in main, output hello message too.

--- src/com/licensetokil/atypistcalendar/ATypistCalendar.java
+++ src/com/licensetokil/atypistcalendar/ATypistCalendar.java
@@ -3,6 +3,8 @@ import com.licensetokil.atypistcalendar.parser.Parser;
 
 public class ATypistCalendar {
 	public static void main(String[] args) {
+		System.out.println("Hello! Welcome to  A typist calendar!");
 		System.out.println(Parser.Parse("add swimming at CommunityClub on 21/11 from 1300 to 1400"));
+		System.out.println(Parser.Parse("display on 21/11 from 1300 to 1400"));
 	}
 }
