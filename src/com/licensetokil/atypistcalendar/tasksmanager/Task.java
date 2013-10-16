74446beffa175feb08dbe2a1c9a8372177820c12
Steven Kester <a0080415@nus.edu.sg>
2013-10-16 14:11:10 +0800

    updated add parser to be able to add todos and deadlines

---
+++ src/com/licensetokil/atypistcalendar/tasksmanager/Task.java
@@ -0,0 +1,21 @@
+package com.licensetokil.atypistcalendar.tasksmanager;
+
+abstract class Task {
+	protected String taskType;
+	protected int uniqueID;
+	protected String description;
+
+	
+	public String getTaskType(){
+		return taskType;
+	}
+
+	public String getDescription(){
+		return description;
+	}
+	
+	public int getUniqueID(){
+		return uniqueID;
+	}
+	
+}
