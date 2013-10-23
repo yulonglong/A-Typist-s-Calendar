40e58636ef28f195c64209dfdda744373b1dd58c
Lacie <Lacie@localhost>
2013-10-23 14:32:10 +0800

    updated bug in task manager

---
+++ src/com/licensetokil/atypistcalendar/parser/GoogleAction.java
@@ -0,0 +1,32 @@
+package com.licensetokil.atypistcalendar.parser;
+
+public class GoogleAction extends Action{
+	private GoogleActionType type;
+	private String userInput;
+
+	public GoogleAction(){
+		type = null;
+		userInput = new String();
+	}
+	
+	public String toString(){
+		return ("Type	: " + type + "\n" +
+		        "userInput	: " + userInput + "\n");
+	}
+
+	public GoogleActionType getType(){
+		return type;
+	}
+
+	public String getUserInput(){
+		return userInput;
+	}
+
+	public void setType(GoogleActionType newActionType){
+		type = newActionType;
+	}
+
+	public void setUserInput(String newUserInput){
+		userInput = newUserInput;
+	}
+}
