d046936a2c88e7f61bb00a418a304a1c5441c971
Steven Kester <a0080415@nus.edu.sg>
2013-11-03 23:20:58 +0800

    included author name for all files in parser package

--- src/com/licensetokil/atypistcalendar/parser/ExitAction.java
+++ src/com/licensetokil/atypistcalendar/parser/ExitAction.java
@@ -1,3 +1,8 @@
+//@author A0080415N
+//Name       : Steven Kester Yuwono
+//Matric No. : A0080415N
+//com.licensetokil.atypistcalendar.parser
+
 package com.licensetokil.atypistcalendar.parser;
 
 public class ExitAction extends SystemAction{
