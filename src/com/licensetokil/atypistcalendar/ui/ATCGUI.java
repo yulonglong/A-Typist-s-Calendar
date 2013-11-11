c31e6de9bc74f91b3f08884f2b9fa4e6d5589f1e
ian <ian@localhost>
2013-11-11 22:27:46 +0800

    Final changes. Collated files.

--- src/com/licensetokil/atypistcalendar/ui/ATCGUI.java
+++ src/com/licensetokil/atypistcalendar/ui/ATCGUI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 //@author A0103494J
 package com.licensetokil.atypistcalendar.ui;
 
